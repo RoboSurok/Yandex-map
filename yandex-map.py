f6e901452d3ffd2622202e20df6e3b46a2cbc9cb
RoboSurok <isaenkovaleks@mail.ru>
2025-03-02 20:50:35 +0300

    Update yandex-map.py
    Добавлена функция масштабирования

--- yandex-map.py
+++ yandex-map.py
@@ -4,6 +4,7 @@ import sys
 import requests
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtCore import Qt
 
 SCREEN_SIZE = [600, 450]
 
@@ -11,14 +12,15 @@ SCREEN_SIZE = [600, 450]
 class Example(QWidget):
     def __init__(self):
         super().__init__()
+        self.ll = [37.530887, 55.703118]
+        self.spn = [0.002, 0.002]
         self.getImage()
         self.initUI()
 
     def getImage(self):
         server_address = 'https://static-maps.yandex.ru/v1?'
         api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
-        ll_spn = 'll=37.530887,55.703118&spn=0.002,0.002'
-
+        ll_spn = '&'.join(['ll=' + ','.join(map(str, self.ll)), 'spn=' + ','.join(map(str, self.spn))])
         map_request = f"{server_address}{ll_spn}&apikey={api_key}"
         response = requests.get(map_request)
 
@@ -44,6 +46,19 @@ class Example(QWidget):
 
     def closeEvent(self, event):
         os.remove(self.map_file)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_PageUp:
+            if self.spn[0] == 1.0 or self.spn[1] == 1.0:
+                return
+            self.spn = [el + 0.001 for el in self.spn]
+        if event.key() == Qt.Key.Key_PageDown:
+            if self.spn[0] == 0.001 or self.spn[1] == 0.001:
+                return
+            self.spn = [el - 0.001 for el in self.spn]
+        self.getImage()
+        self.pixmap = QPixmap(self.map_file)
+        self.image.setPixmap(self.pixmap)
 
 
 if __name__ == '__main__':
